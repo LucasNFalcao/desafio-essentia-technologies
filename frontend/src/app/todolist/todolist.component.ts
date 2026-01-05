aac5c1a17d8f9d02da3adfc48e16402f30419c31
Lucas Falcão <lucasfalcao23@hotmail.com>
2026-01-04 21:04:58 -0300

    feat: Create the todolist CRUD on frontend side

--- frontend/src/app/todolist/todolist.component.ts
+++ frontend/src/app/todolist/todolist.component.ts
@@ -1,9 +1,58 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-todolist',
-  imports: [],
+  imports: [FormsModule, NgClass],
   templateUrl: './todolist.component.html',
   styleUrl: './todolist.component.css',
 })
-export class Todolist {}
+export class Todolist {
+  taskArray = [
+    {
+      id: 1,
+      title: 'Sample Task',
+      description: 'This is a sample task description.',
+      isCompleted: false,
+      isReadOnly: true,
+    },
+    {
+      id: 2,
+      title: 'Another Sample Task',
+      description: 'This is an another sample task description.',
+      isCompleted: false,
+      isReadOnly: true,
+    },
+  ];
+
+  onCreateTask(form: NgForm) {
+    this.taskArray.push({
+      id: this.taskArray.length + 1,
+      title: form.value.titleTask,
+      description: form.value.textTask,
+      isCompleted: false,
+      isReadOnly: true,
+    });
+
+    form.reset();
+  }
+
+  onEditTask(index: number) {
+    this.taskArray[index].isReadOnly = !this.taskArray[index].isReadOnly;
+  }
+
+  onUpdateTask(index: number, form: NgForm) {
+    this.taskArray[index].title = form.value.titleTask;
+    this.taskArray[index].description = form.value.textTask;
+  }
+
+  onDeleteTask(index: number) {
+    this.taskArray.splice(index, 1);
+  }
+
+  onCheckboxChange(index: number) {
+    this.taskArray[index].isCompleted = !this.taskArray[index].isCompleted;
+    console.log(this.taskArray[index].isCompleted);
+  }
+}
